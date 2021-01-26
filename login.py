122ab0f1eea3504e1ae02d58d22ebb25c8a9e99a
Jmmxp <Jmmxp@users.noreply.github.com>
2021-01-25 20:21:19 -0500

    print out env variables on login page

--- login.py
+++ login.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-from templates import *
+import os
+from templates import login_page, _wrapper
 
 print("Content-Type: text/html")
 print()
-print(login_page())
+
+build = login_page() + f"<p>QUERY_STRING: {os.environ['QUERY_STRING']}</p>"
+build += f"<p>{os.environ}</p>"
+
+print(_wrapper(build))
+
