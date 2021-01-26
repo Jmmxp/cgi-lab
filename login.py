2142a2a25658ec656c23fc2cbe7bfb16d53f6c38
Jmmxp <Jmmxp@users.noreply.github.com>
2021-01-25 20:28:51 -0500

    working printing of submitted user and pass

--- login.py
+++ login.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 import os
+import sys
 from templates import login_page, _wrapper
 
 print("Content-Type: text/html")
 print()
 
-build = login_page() + f"<p>QUERY_STRING: {os.environ['QUERY_STRING']}</p>"
-build += f"<p>{os.environ}</p>"
+build = login_page()
+
+length = os.environ["CONTENT_LENGTH"]
+
+if length.strip() != "":
+    data = ""
+    for char in sys.stdin.read(int(length)):
+        data += char
+
+    params = data.split("&")
+    for param in params:
+        k, v = param.split("=")
+        build += f"{k}: {v}\n"
 
 print(_wrapper(build))
 
