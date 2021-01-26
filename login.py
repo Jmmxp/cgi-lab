e8d345b6364b67fb97acbbac02e8431c1d333e8b
Jmmxp <Jmmxp@users.noreply.github.com>
2021-01-25 20:03:17 -0500

    Use templates login_page method

--- login.py
+++ login.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
-import os
-import json
+from templates import *
 
 print("Content-Type: text/html")
 print()
-print("""
-<!DOCTYPE html>
-<html>
-<body>
-""")
-
-print(f"<p>QUERY_STRING: {os.environ['QUERY_STRING']}</p>")
-for parameter in os.environ["QUERY_STRING"].split("&"):
-    (name, value) = parameter.split("=")
-    print(f"<li><em>{name} = {value} </em></li>")
-
-print("""
-<ul>
-</ul>
-</body>
-</html>
-""")
+print(login_page())
